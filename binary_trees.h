bde7ed48b1f4349c6a4c6deee6f592504b286936
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-03 21:00:06 +0000

    made amends to all mandatory tasks

--- binary_trees.h
+++ binary_trees.h
@@ -62,9 +62,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 /* Function to count the nodes with at least 1 child in a binary tree */
 size_t binary_tree_nodes(const binary_tree_t *tree);
 
-/* Function to measure the height of a binary tree */
-size_t binary_tree_height(const binary_tree_t *tree);
-
 /* Function to measure the balance factor of a binary tree */
 int binary_tree_balance(const binary_tree_t *tree);
 
