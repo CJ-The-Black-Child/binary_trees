d1e94957963234831d7f9d8e42c5573cd2c16475
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-04 10:32:23 +0000

    updated task #1 to meet qa review expectation

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,8 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	binary_tree_t *new_node;
+	new_node = binary_tree_node(parent, value);
 
 	if (parent == NULL)
 		return (NULL);
@@ -21,6 +22,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (new_node == NULL)
 		return (NULL);
+
+	new_node->left = parent->left;
 
 	if (parent->left != NULL)
 	{
