dff00f86088bec55d2f6f3fa80f4bd2377a02a5c
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-04 00:39:30 +0000

    task #39. Heap - Extract

---
+++ 133-heap_extract.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - Extracts the root node of a Max Binary Heap
+ * @root: Double pointer to the root node of the heap
+ * Return: The value stored in the root node, or 0 on failure
+ */
+int heap_extract(heap_t **root)
+{
+	if (root == NULL || *root == NULL)
+		return (0);
+
+	int value = (*root)->n;
+	heap_t *last_node = find_last_node(*root);
+
+	if (last_node == *root)
+	{
+		free(*root);
+		*root = NULL;
+		return (value);
+	}
+
+	swap_nodes(*root, last_node);
+	if (last_node->parent->right == last_node)
+		last_node->parent->right = NULL;
+	else
+		last_node->parent->left = NULL;
+
+	free(last_node);
+	heapify_down(*root);
+
+	return (value);
+}
