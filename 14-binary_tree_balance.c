5bd98ab0461da76e204fe1a1b5acccc99fbf335e
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-04 15:34:10 +0000

    updated task #14 to meet the betty coding, syntax and styling documentation

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,13 +7,28 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (0);
+}
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+/**
+ * binary_tree_height - it measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree that measures the height.
+ *
+ * Return: If tree is NULL, then function must return 0, otherwise its
+ * returning the height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	return (left_height - right_height);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
